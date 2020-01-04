c5b51e0d3342b7c7d5ba95eea4ca7598a04221f4
Alan Dega <arkidega@gmail.com>
2020-01-03 19:31:42 -0600

    c1

---
+++ src/components/Menu.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+ 
+export default function Menu(props) {
+    return (
+        <nav id="menu">
+           <Link to='/'>
+               Usuarios
+           </Link>
+           <Link to='/tareas'>
+               Tareas
+           </Link>
+        </nav>
+    )
+}
+
+
